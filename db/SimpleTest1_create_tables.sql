297d9db3127c9f9c05e9777132c4c0ad4742a984
CAEUser <e37f44db2937b2e33c487273d264a29359dc4138>
2020-10-15 14:23:54 +0000

    Generated microservice version 1.0.0

---
+++ db/SimpleTest1_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service SimpleTest1, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table ta.
+--
+CREATE TABLE caeschema.ta (
+  co INTEGER ,
+CONSTRAINT co_PK PRIMARY KEY (co)
+ 
+);
+
+
+
